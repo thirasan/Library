d92dd51947da0dee82b108f03d33cb9960d9ecc8
Thirasan <thirasan@Thirasans-MacBook-Pro.local>
2017-05-22 13:07:13 +0700

    Complete Return

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,107 @@
 import React, { Component } from 'react'
 import styles from './Home.scss'
+import $ from 'JQuery';
+import axios from 'axios'
 
 class Home extends Component {
+  componentWillMount(){
+        this.bookTable()
+        this.digitalMediaTable()
+
+  }
+
+  bookTable(){
+        axios.get(`http://localhost:3943/userBook/${this.props.params.user}`).then(res => res.data).then((data) => {
+            var columns = [];
+            var headerTr$ = $('<tr/>');
+
+            for (let i = 0 ; i < data.length ; i++) {
+                var rowHash = data[i];
+                for (var key in rowHash) {
+                    if ($.inArray(key, columns) == -1){
+                        columns.push(key);
+                            headerTr$.append($('<th/>').html(key));
+                    }
+                }
+            }
+            $("#excelDataTable").append(headerTr$);
+
+            for (let i = 0 ; i < data.length ; i++) {
+                var row$ = $('<tr/>');
+                for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+                    var cellValue = data[i][columns[colIndex]];
+
+                    if (cellValue == null) { cellValue = ""; }
+                
+                    if (colIndex == 2){
+                        row$.append(`<td><button>Return</button></td>`).click( function(){ 
+                          axios.get(`http://localhost:3943/bookReturn/${data[i][columns[colIndex-6]]}`).then(
+                            function(){
+                              window.location.reload();
+                            })
+                        });
+                    } else
+                        row$.append($('<td/>').html(cellValue));
+                }
+                $("#excelDataTable").append(row$);
+            }
+        })
+  }
+
+  digitalMediaTable(){
+        axios.get(`http://localhost:3943/userDigitalMedia/${this.props.params.user}`).then(res => res.data).then((data) => {
+            var columns = [];
+            var headerTr$ = $('<tr/>');
+
+            for (let i = 0 ; i < data.length ; i++) {
+                var rowHash = data[i];
+                for (var key in rowHash) {
+                    if ($.inArray(key, columns) == -1){
+                        columns.push(key);
+                            headerTr$.append($('<th/>').html(key));
+                    }
+                }
+            }
+            $("#excelDataDigitalTable").append(headerTr$);
+
+            for (let i = 0 ; i < data.length ; i++) {
+                var row$ = $('<tr/>');
+                for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+                    var cellValue = data[i][columns[colIndex]];
+
+                    if (cellValue == null) { cellValue = ""; }
+                
+                    if (colIndex == 2){
+                        row$.append(`<td><button>Return</button></td>`).click( function(){
+                          
+                          axios.get(`http://localhost:3943/digitalMediaReturn/${data[i][columns[colIndex-6]]}`).then(
+                            function(){
+                              window.location.reload();
+                            })
+                        });
+                    } else
+                        row$.append($('<td/>').html(cellValue));
+                }
+                $("#excelDataDigitalTable").append(row$);
+            }
+        })
+  }
+
   render() {
     return (
       <div>
-        <h2 className={styles['title']}>
-          Welcome to Library
-        </h2>
+        <table id="excelDataTable" className={styles['table']}>
+            <thead id="head">
+            </thead>
+            <tbody id="body">
+            </tbody>
+        </table>
+        <table id="excelDataDigitalTable" className={styles['table']}>
+            <thead id="headDigital">
+            </thead>
+            <tbody id="bodyDigital">
+            </tbody>
+        </table>
       </div>
     )
   }
--- src/document/book.js
+++ src/document/book.js
@@ -9,28 +9,84 @@ class Book extends Component {
         super();
 
         this.state = {
-            authorName: '',
-            authorID: ''
+            ID: "",
+            BookName: "",
+            PageCount: "",
+            FirstPublished: "",
+            Award: "",
+            Genre: "",
+            AuthorID: "",
+            AuthorName: ""
         };
     }
     componentWillMount() {
         axios.get(`http://localhost:3943/book/${this.props.params.id}`).then(res => res.data).then((data) => {
-            this.state.authorName = data[0].Author_ID;
-            this.state.authorID = data[0].Author_ID;
-            console.log(data[0].Author_ID)
-            document.getElementById('author').innerHTML = this.state.authorName;
+            this.state.ID = data[0].Book_ID;
+            this.state.BookName = data[0].BookName;
+            this.state.PageCount = data[0].PageCount;
+            this.state.FirstPublished = data[0].FirstPublished;
+            for(var i=0;i<data.length;i++){
+                this.state.Genre += data[i].Genre;
+                if (i<data.length-1) this.state.Genre += ', ';
+            }
+            this.state.AuthorID = data[0].author_ID;
+            this.state.AuthorName = data[0].authorName;
+            
+            document.getElementById('ID').innerHTML = 'Book ID: '+this.state.ID;
+            document.getElementById('BookName').innerHTML = 'Book name: ' +this.state.BookName;
+            document.getElementById('PageCount').innerHTML = 'Number of pages: '+this.state.PageCount;
+            document.getElementById('FirstPublished').innerHTML = 'First published: '+ this.state.FirstPublished;
+            // document.getElementById('Award').innerHTML = 'Award: '+this.state.Award;
+            document.getElementById('Genre').innerHTML = 'Genre(s): ' + this.state.Genre;
+            document.getElementById('AuthorName').innerHTML = 'Author: ' + this.state.AuthorName;
+            axios.get(`http://localhost:3943/bookAward/${this.props.params.id}`).then(res => res.data).then((data) => {
+                if (data[0].AwardName != null){
+                    for(var i=0;i<data.length;i++){
+                        this.state.Award += data[i].AwardName + " " + data[i].Year;
+                        if (i<data.length-1) this.state.Award += ', ';
+                    }
+                }
+                    
+                document.getElementById('Award').innerHTML = 'Award: '+this.state.Award;
+            })
         })
     }
     linkToAuthor() {
-        browserHistory.push(`/${this.props.user}/author/${this.state.authorID}`);
+        browserHistory.push(`/${this.props.params.user}/author/${this.state.AuthorID}`);
     }
+    borrowBook() {
+        axios.post('http://localhost:3943/borrowBook',{
+            user: this.props.params.user,
+            bookID: this.state.ID
+        }).then(res => res.data).then((data) => {
 
+        })
+    }
 
     render() {
         return (
             <div>
-                <button id = "author" onClick={(e) => this.linkToAuthor(e)}>Loading</button>
-                <div>{this.props.params.user}</div>
+                <div id="ID">
+                    Loading
+                </div>
+                <div id="BookName">
+                    Loading
+                </div>
+                <div id="Genre">
+                    Loading
+                </div>
+                <button id = "AuthorName" onClick={(e) => this.linkToAuthor(e)}>Loading</button>
+                <div id="PageCount">
+                    Loading
+                </div>
+                <div id="FirstPublished">
+                    Loading
+                </div>
+                <div id="Award">
+                    Loading
+                </div>
+                <button onClick={(e) => this.borrowBook(e)}>Borrow</button>
+                <div>{"User : " + this.props.params.user}</div>
                 <img src={require(`./bookimages/${this.props.params.id}.jpg`)}/>
             </div>
         )
